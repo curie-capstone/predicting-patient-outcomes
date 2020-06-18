f726ef08753cb33a1a3f76bdb28f42cf24606d9c
shayaltshue <shayaltshue@gmail.com>
2020-06-18 14:45:08 -0500

    reached checkpoint with wrangling

--- shay/wrangle.py
+++ shay/wrangle.py
@@ -2,11 +2,18 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
+# Acquiring the Data
 def get_data():
     # Get the data into a pandas dataframe
     df =  pd.read_csv('data/training_v2.csv')
     print(f'rows: {df.shape[0]}, columns: {df.shape[1]}')
     return df
+
+
+# Dtype Tools
+def convert_to_int_col(col):
+    col = col.astype(int)
+    
 
 def drop_cols_and_rows_by_threshold(df, prop_required_column = .2, prop_required_row = .4):
     shape_before = df.shape
@@ -14,19 +21,47 @@ def drop_cols_and_rows_by_threshold(df, prop_required_column = .2, prop_required
     df.dropna(axis=1, thresh=threshold, inplace=True)
     threshold = int(round(prop_required_row*len(df.columns),0))
     df.dropna(axis=0, thresh=threshold, inplace=True)
-    shape_after = df.shape
+    shape_aßfter = df.shape
     rows = shape_before[0] - shape_after[0]
     cols = shape_before[1] - shape_after[1]
     print(f'\t * Number of patients dropped: {rows} \n \t * Number of features dropped:{cols}')
     return df
+
     
 def fill_with_mode(df):
     cols = ['age', 'bmi',
-            'hospital_admit_source']
+            'ethnicity']
     for col in cols:
-        df[col].fillna(df[col].mode(), inplace=True)
-        print('    * ' + col)
+        if df[col].dtype == float:
+            df[col].fillna(int(df[col].mode()), inplace=True)
+        else:
+            df[col].fillna(df[col].mode()[0], inplace=True)
+        print('\t * ' + col)
     return df
+
+
+def drop_rows(df):
+    print('\t * Dropping patients with no recorded gender')
+    num_patients = df.gender.isna().sum()
+    df.dropna(subset=['gender'], inplace=True)
+    print(f'\t\t - Number of patients dropped: {num_patients}')
+    return df
+
+
+def fill_with_zero(df):
+    cols = ['arf_apache', 'intubated_apache',
+            'cirrhosis', 'aids', 'diabetes_mellitus',
+            'hepatic_failure', 'immunosuppression',
+            'leukemia', 'lymphoma', 
+            'solid_tumor_with_metastasis',
+            'hospital_death', 'ethnicity',
+            'gender', 'age', 'bmi']
+    
+    for col in cols:
+        df[col].fillna(0, inplace=True)
+        print('\t * ' + col)
+    return df
+        
 
 # Main Function
 def get_training_data():
@@ -36,12 +71,14 @@ def get_training_data():
     print('---Acquiring the Data---')
     df = get_data()
     
-    # Apply data transformations through helper functions
     print('\n')
+    
     print('---Handling Missing Values---')
     print('Filling nulls with mode for the following features:')
     df.pipe(fill_with_mode)
-    print('Dropping features and patients with too many missing values')
-    df.pipe(drop_cols_and_rows_by_threshold)
+    print('Handling nulls within rows')
+    df.pipe(drop_rows)
+    print('Filling nulls with 0 (aka False) for the following columns')
+    df.pipe(fill_with_zero)
     return df
     
