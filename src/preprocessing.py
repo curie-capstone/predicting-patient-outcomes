664155784282a8adee7d6fdb47dd160f98a9cb44
Shay Altshue <shayaltshue@users.noreply.github.com>
2020-06-24 12:25:18 -0500

    Added splitting data

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,14 +1,25 @@
 import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 
+
+def split_data(df):
+    X = df.drop(columns="hospital_death")
+    y = df.hospital_death
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, train_size=0.8, random_seed=42, stratify=stratify_on
+    )
+    return X_train, X_test, y_train, y_test
+
+
 def encode(df, l):
-    '''
+    """
     OneHot encodes the data in a column or columns
-    '''
-    encoder = OneHotEncoder(sparse = False)
+    """
+    encoder = OneHotEncoder(sparse=False)
     encoder.fit(df[l])
     m = encoder.transform(df[l])
-    col_name= encoder.get_feature_names(l)
-    df = pd.concat([df, pd.DataFrame(m, columns = col_name,index = df.index)], axis =1)
-    df = df.drop(columns = l)
+    col_name = encoder.get_feature_names(l)
+    df = pd.concat([df, pd.DataFrame(m, columns=col_name, index=df.index)], axis=1)
+    df = df.drop(columns=l)
     return df
